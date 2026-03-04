53ea7919acda2acc20cdf5669bb207611cdb6b8e
NatalieGichuki <nataliegichuki@gmail.com>
2026-03-05 02:59:19 +0300

    Deleted extra files

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-#Entry point for running the flask application
-
-app = create_app("development")
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5555)
