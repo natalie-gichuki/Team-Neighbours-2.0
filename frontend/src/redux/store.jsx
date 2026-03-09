bd151d0fe4b0edd4aa7e53dc11fe9b6d363caab3
NatalieGichuki <nataliegichuki@gmail.com>
2026-03-09 23:20:29 +0300

    the contribution slice

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Slices/authSlice';
+import contributionsReducer from './Slices/contributionsSlice';
 // This is the redux store configuration file 
 
 
@@ -7,6 +8,7 @@ const store = configureStore({
     // The reducer is set to authReducer, which handles authentication state.
     reducer: {
         auth: authReducer,
+        contributions: contributionsReducer,
     },
     // The devTools option is enabled in development mode to allow for easier debugging of the Redux state.
     devTools: process.env.NODE_ENV !== 'production',
