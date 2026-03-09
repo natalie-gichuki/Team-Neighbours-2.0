8d7fcaa192211d9ad113b3e2140b553b962fcb39
NatalieGichuki <nataliegichuki@gmail.com>
2026-03-09 23:17:11 +0300

    Creating the member contributions page

---
+++ frontend/src/pages/Contributions.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContributionById } from "../redux/Slices/contributionsSlice";
+
+const Contributions = () => {
+
+    const dispatch = useDispatch();
+
+    const { contributions, loading, error } = useSelector(
+        (state) => state.contributions
+    );
+
+    const user = JSON.parse(localStorage.getItem("user")) || {};
+
+    useEffect(() => {
+        if (user && user.id){
+            dispatch(fetchContributionById(user.id));
+        }
+        
+    }, [dispatch, user?.id]);
+
+    const myContributions = Array.isArray(contributions)
+        ? contributions.filter(c => c.member_id === user.id) 
+        : [];
+
+    return (
+        <div>
+            <h2>My Contributions</h2>
+
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+
+            <table border="1">
+
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {myContributions.map((contribution) => (
+                        <tr key={contribution.id}>
+                            <td>{contribution.id}</td>
+                            <td>{contribution.amount}</td>
+                            <td>{contribution.date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+
+            </table>
+        </div>
+    );
+};
+
+export default Contributions;
