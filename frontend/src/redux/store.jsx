399c67a6e79a7849eafa34180b5223838e1d72c9
NatalieGichuki <nataliegichuki@gmail.com>
2026-03-12 01:21:50 +0300

    Registering the member reducer

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Slices/authSlice';
 import contributionsReducer from './Slices/contributionsSlice';
+import membersReducer from './Slices/membersSlice';
 // This is the redux store configuration file 
 
 
@@ -9,6 +10,7 @@ const store = configureStore({
     reducer: {
         auth: authReducer,
         contributions: contributionsReducer,
+        members: membersReducer
     },
     // The devTools option is enabled in development mode to allow for easier debugging of the Redux state.
     devTools: process.env.NODE_ENV !== 'production',
