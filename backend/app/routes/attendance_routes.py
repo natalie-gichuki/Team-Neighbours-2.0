0f4d43db2da7b6ae9241cb3363704b1cdb2a77bf
NatalieGichuki <nataliegichuki@gmail.com>
2026-03-12 04:54:36 +0300

    Adding the route to enable a member to get a list of all his/her attendance records

--- backend/app/routes/attendance_routes.py
+++ backend/app/routes/attendance_routes.py
@@ -4,6 +4,9 @@ from app.models.attendance import Attendance
 from flasgger.utils import swag_from
 from datetime import datetime
 from app.utils.auth_helpers import role_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.members import Member
+
 
 attendance_bp = Blueprint('attendance', __name__)
 
@@ -237,3 +240,138 @@ def delete_attendance(attendance_id):
     db.session.commit()
 
     return jsonify({"msg": "Attendance record deleted successfully."}), 200
+
+
+@attendance_bp.route('/attendance/<int:attendance_id>', methods=['PATCH'])
+@swag_from({
+    'tags': ['Attendance'],
+    'description': 'Update a attendance record',
+    'security': [{'Bearer': []}],
+    'parameters': [
+        {
+            'name': 'attendance_id',
+            'in': 'path',
+            'required': True,
+            'type': 'integer'
+        },
+        {
+            'name': 'body',
+            'in': 'body',
+            'required': True,
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'member_id': {'type': 'integer'},
+                    'date': {'type': 'string', 'format': 'date'},
+                    'status': {'type': 'string', 'enum': ['present', 'absent', 'late']}
+                }
+            }   
+        }
+    ],
+    'responses': {
+        200: {
+            'description': 'Attendance record updated successfully',
+            'examples': {
+                'application/json': {
+                    'msg': 'Attendance record updated successfully'
+                }
+            }
+        },
+        404: {
+            'description': 'Attendance not found',
+            'examples': {
+                'application/json': {
+                    'msg': 'Attendance not found'
+                }
+            }
+        }
+    }
+})
+@role_required('admin')
+def update_attendance(attendance_id):
+    if request.method == 'OPTIONS':
+        return '', 200
+    
+    attendance = Attendance.query.get(attendance_id)
+
+    if not attendance:
+        return jsonify({"msg": "Attendance record not found"})
+    
+    data = request.get_json()
+
+    member_id = data.get('member_id')
+    date_str = data.get('date')
+    status = data.get('status')
+
+    if member_id:
+        attendance.member_id = member_id
+    if date_str:
+        try:
+            attendance.date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        except ValueError:
+            return jsonify({"msg": "Date must be in YYYY-MM-DD format"}), 400
+    if status:
+        attendance.status = status
+
+    db.session.commit()
+
+    return jsonify({"msg": "Attendance record updated successfully."}), 200
+
+
+@attendance_bp.route('/attendance/my', methods=['GET'])
+@swag_from({ 
+    'tags': ['Attendance'],
+    'description': 'Get attendance records for the logged-in member',
+    'security': [{'Bearer': []}],
+    'responses': {
+        200: {
+            'description': 'attendance records retrieved successfully',
+            'schema': {
+                'type': 'array',
+                'items': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'integer'},
+                        'member_id': {'type': 'integer'},
+                        'date': {'type': 'string', 'format': 'date'},
+                        'status': {'type': 'string'}
+                    }
+                }
+            },
+            'examples': {
+                'application/json': [
+                    {
+                        'id': 1,
+                        'member_id': 1,
+                        'date': '2023-10-01',
+                        'status': 'present'
+                    },
+                    {
+                        'id': 2,
+                        'member_id': 1,
+                        'date': '2023-10-02',
+                        'status': 'absent'
+                    }
+                ]
+            }
+        },
+        404: {
+            'description': 'Member not found',
+            'examples': {
+                'application/json': {
+                    'msg': 'Member not found'
+                }
+            }
+        }
+    }
+})
+@jwt_required()
+@role_required('admin', 'member')
+def get_my_attendance():
+    # get logged-in user id from JWT
+    member_id = get_jwt_identity()  # JWT stores member.id
+
+    # get contributions for that member
+    attendances = Attendance.query.filter_by(member_id=member_id).all()
+
+    return jsonify([c.to_dict() for c in attendances]), 200
