8604afd7ee475f6e644b614764ffed494c4aff70
NatalieGichuki <nataliegichuki@gmail.com>
2026-03-12 05:10:11 +0300

    Importing os to help with JWTsecret key

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from datetime import timedelta
+import os
 
 from dotenv import load_dotenv
 load_dotenv()
