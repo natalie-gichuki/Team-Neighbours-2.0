755e24128438fbe57b9969d94f40bd5a2c58b68e
NatalieGichuki <nataliegichuki@gmail.com>
2026-03-13 21:54:40 +0300

    Registering the loans reducer

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -4,6 +4,7 @@ import contributionsReducer from './Slices/contributionsSlice';
 import membersReducer from './Slices/membersSlice';
 import attendanceReducer from './Slices/attendanceSlice';
 import fineReducer from './Slices/fineSlice';
+import loanReducer from './Slices/loanSlice'
 // This is the redux store configuration file 
 
 
@@ -15,6 +16,7 @@ const store = configureStore({
         members: membersReducer,
         attendance: attendanceReducer,
         fine: fineReducer,
+        loan: loanReducer
     },
     // The devTools option is enabled in development mode to allow for easier debugging of the Redux state.
     devTools: process.env.NODE_ENV !== 'production',
